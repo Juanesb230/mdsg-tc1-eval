5d030d14fccd7c3699869db6576f03ba4fe4b7cb
Lucas <lusantil@pichincha.com>
2023-04-14 18:50:45 -0300

    fixing troubles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from "react";
 import "./App.css";
 import Scroll from "./components/atoms/scroll/scroll";
@@ -12,29 +11,6 @@ function App() {
         <Input onChange={(e) => setFilter(e.target.value)} />
       </header>
       <Scroll filter={filter} />
-=======
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
->>>>>>> e46627b (Initialize project using Create React App)
     </div>
   );
 }
