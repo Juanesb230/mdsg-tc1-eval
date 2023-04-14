60981dd46712bcb406aeca0f5a65e58a487067de
Diego Márquez <57378696+oddyEC@users.noreply.github.com>
2023-04-14 11:30:46 -0500

    mockeando para pruebas

---
+++ pokemon-app/src/utils/constants/pokemon.ts
@@ -0,0 +1,6 @@
+import Pokemon from '../interfaces/pokemon';
+
+export const mockedPokemon: Pokemon[] = [	
+    { name: 'bulbasaur', url: 'https://pokeapi.co/api/v2/pokemon/1/' },
+    { name: 'ivysaur', url: 'https://pokeapi.co/api/v2/pokemon/2/' },
+];
